b4777e7b68a77d65ef284b2f66f003027001ac4d
zy <myaccountzy@163.com>
2023-04-05 21:52:15 +0800

    3-6 新增ebook表,生成持久层代码

---
+++ src/main/java/net/siehe/wiki/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package net.siehe.wiki.mapper;
+
+import java.util.List;
+import net.siehe.wiki.domain.Ebook;
+import net.siehe.wiki.domain.EbookExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
