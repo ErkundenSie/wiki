26337869dbb4e2cbee972214c5edde6852b409f3
zy <myaccountzy@163.com>
2023-04-05 17:24:54 +0800

    2-8 增加自定义配置示例

--- src/main/java/net/siehe/wiki/controller/TestController.java
+++ src/main/java/net/siehe/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package net.siehe.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 //设置Auto Import==》Optimize imports on the fly打钩自动删除没用的
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.*;
 //RestController返回字符串 Controller返回页面，一般前后端分离用前者
 @RestController
 public class TestController {
+    //应用自定义配置项设置默认值TEST
+//    @Value("${test.hello:TEST}")
+    @Value("${test.hello:TEST}")
+    private String testHello;
     /**
      * 常用GET查,POST增,PUT改,DELETE删
      * 传统/user?id=1
@@ -22,7 +27,7 @@ public class TestController {
     //浏览器直接访问发送的是GET请求用POST请求会报405错误
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World , "+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name) {
