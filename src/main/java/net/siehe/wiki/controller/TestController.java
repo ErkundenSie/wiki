ee1d0256a18fc8cd4a3b6a5ad8715278a3335fe3
zy <myaccountzy@163.com>
2023-04-05 16:56:48 +0800

    2-7 增加hello接口接口测试test.http

--- src/main/java/net/siehe/wiki/controller/TestController.java
+++ src/main/java/net/siehe/wiki/controller/TestController.java
@@ -2,6 +2,9 @@ package net.siehe.wiki.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+//设置Auto Import==》Optimize imports on the fly打钩自动删除没用的
+//import java.util.Map;
+
 //RestController返回字符串 Controller返回页面，一般前后端分离用前者
 @RestController
 public class TestController {
@@ -20,5 +23,9 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post" + name;
     }
 }
