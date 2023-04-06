05e1e2742e963ad3c0b313d332690822a476fa49
zy <myaccountzy@163.com>
2023-04-06 17:54:44 +0800

    5-4 集成图标组件, npm install @ant-design/icons-vue@5.1.8 --save

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 //引入
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
-
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+//导入所有图标1
+import * as Icons from '@ant-design/icons-vue';
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app')
+//全局使用图标2，注意const app和分段
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -46,8 +46,42 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            <pre>{{ ebooks }}{{ ebooks2 }}</pre><!--pre标签就是会把里面长什么样含空格,原封不动的全部给你展示到页面上-->
+<!--            <pre>{{ ebooks }}{{ ebooks2 }}</pre>&lt;!&ndash;pre标签就是会把里面长什么样含空格,原封不动的全部给你展示到页面上&ndash;&gt;-->
+            <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+                <template #footer>
+                    <div>
+                        <b>ant design vue</b>
+                        footer part
+                    </div>
+                </template>
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.title">
+                        <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+                        </template>
+                        <template #extra>
+                            <img
+                                width="272"
+                                alt="logo"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                            />
+                        </template>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.title }}</a>
+                            </template>
+                            <template #avatar><a-avatar :src="item.avatar" /></template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
+
         </a-layout-content>
+
     </a-layout>
 </template>
 
@@ -56,6 +90,19 @@ import {defineComponent, onMounted, reactive, ref, toRef,} from 'vue';
 //导入集成HTTP库Axios
 import axios from 'axios';
 
+const listData: any = [];
+
+for (let i = 0; i < 23; i++) {
+    listData.push({
+        href: 'https://www.antdv.com/',
+        title: `ant design vue part ${i}`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+}
 export default defineComponent({
     name: 'HomeView',
     // setup() 初始化化方法 Vue2.0中的data()、mounted()、methods()被setup()取代
@@ -84,7 +131,19 @@ export default defineComponent({
         //返回数据
         return {
             ebooks,
-            ebooks2:toRef(ebooks1,"books")  //toRef把books变成响应式变量,定义变量名ebooks2
+            ebooks2: toRef(ebooks1, "books"), //toRef把books变成响应式变量,定义变量名ebooks2
+            listData,
+            pagination: {
+                onChange: (page: any)=> {
+                    console.log(page);
+                },
+                pageSize: 3,
+            },
+            actions: [
+                {type: 'StarOutlined', text: '156'},
+                {type: 'LikeOutlined', text: '156'},
+                {type: 'MessageOutlined', text: '2'},
+            ],
         }
     }
 });
