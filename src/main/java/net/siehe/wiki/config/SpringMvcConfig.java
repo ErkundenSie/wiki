ac84c6217c988fca39d4d5accbeaab5fc5083ada
zy <myaccountzy@163.com>
2023-04-06 21:16:05 +0800

    5-9 增加AOP,打印接口耗时, 请求参数和返回数去掉过滤器、拦截器

--- src/main/java/net/siehe/wiki/config/SpringMvcConfig.java
+++ src/main/java/net/siehe/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,20 @@
-package net.siehe.wiki.config;
-
-import net.siehe.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-   @Resource
-   LogInterceptor logInterceptor;
-
-   public void addInterceptors(InterceptorRegistry registry) {
-       registry.addInterceptor(logInterceptor)
-               .addPathPatterns("/**") //针对所有请求
-               .excludePathPatterns("/log");//排除登录接口
-   }
-}
+//package net.siehe.wiki.config;
+//
+//import net.siehe.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//   @Resource
+//   LogInterceptor logInterceptor;
+//
+//   public void addInterceptors(InterceptorRegistry registry) {
+//       registry.addInterceptor(logInterceptor)
+//               .addPathPatterns("/**") //针对所有请求
+//               .excludePathPatterns("/log");//排除登录接口
+//   }
+//}
