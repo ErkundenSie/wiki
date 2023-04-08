df9009e8203fdd3ef300fb2c88b1fc10a0b6d09f
zy <myaccountzy@163.com>
2023-04-08 10:46:10 +0800

    6-12 增加按名字查询电子书功能 ① 添加查询框<a-form layout="inline" :model="param"> ②将输入的内容放到param.name,返回后端 ③定义param

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,10 +3,23 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            <p>
-                <a-button type="primary" @click="add()" size="large">
-                    新增
-                </a-button>
+            <p><!--添加查询框-->
+                <a-form layout="inline" :model="param"><!--调用param-->
+                    <a-form-item>
+                        <a-input v-model:value="param.name" placeholder="名称">
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+                            查询
+                        </a-button>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button type="primary" @click="add()">
+                            新增
+                        </a-button>
+                    </a-form-item>
+                </a-form>
             </p>
 
             <a-table
@@ -77,6 +90,8 @@ import {message} from "ant-design-vue";
 export default defineComponent({
     name: 'AdminEbook',
     setup() {
+        const param = ref();
+        param.value = {}; //定义并初始化param
         const ebooks = ref();
         //分页
         const pagination = ref({
@@ -133,6 +148,7 @@ export default defineComponent({
                 params: {
                     page: params.page,
                     size: params.size,
+                    name: param.value.name  //前端输入的名字传递到后端从响应式param拿到
                 }
             }).then((response) => {
                     loading.value = false;
@@ -227,11 +243,13 @@ export default defineComponent({
 
         return {
             //表格的
+            param,
             ebooks,
             pagination,
             columns,
             loading,
             handleTableChange,
+            handleQuery,
 
             edit,
             add,
