10e6792d731262e69d0ba70fad2c70af3e3df245
zy <myaccountzy@163.com>
2023-04-12 16:53:41 +0800

    7-7 加载完分类handleQueryCategory后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -196,7 +196,6 @@ export default defineComponent({
                 const data = response.data; // data = commonResp
                 if (data.success) {
                     modalVisible.value = false;
-
                     // 重新加载列表
                     handleQuery({
                         page: pagination.value.current,
@@ -258,7 +257,7 @@ export default defineComponent({
                     level1.value = Tool.array2Tree(categorys, 0);
                     console.log("树形结构：", level1.value);
 
-                    // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+                    // 加载完分类handleQueryCategory后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
                     handleQuery({
                         page: 1,
                         size: pagination.value.pageSize,
